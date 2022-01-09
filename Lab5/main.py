4f022bc981336e334c0464ce3f8d5219cfbc640b
ddbl <daviddeyv@gmail.com>
2022-01-09 19:31:33 +0200

    add last lab

---
+++ Lab5/main.py
@@ -0,0 +1,22 @@
+# 1a
+# Class Grammar (required operations: read a grammar from file, print set of nonterminals,
+# set of terminals, set of productions, productions for a given nonterminal, CFG check)
+#
+# Input files: g1.txt (simple grammar from course/seminar),
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
